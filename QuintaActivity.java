e90b807b3a6bd1dee9956352511033416acd5731
jose-z-m <65821332+jose-z-m@users.noreply.github.com>
2020-06-14 22:17:39 -0500

    Create QuintaActivity.java

---
+++ QuintaActivity.java
@@ -0,0 +1,16 @@
+# Proyecto_TM
+RODRIGOCHINCHAY,JAMESCARBAJAL,KELLYVERONICA,JOSEZUÑIGA
+package com.example.proyecto_tm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuintaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quinta);
+    }
+}
