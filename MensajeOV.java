38f7399ec0b9152dc8b5d867e2b7ee486a1b8d75
jose-z-m <65821332+jose-z-m@users.noreply.github.com>
2020-06-14 22:29:30 -0500

    Create MensajeOV.java

---
+++ MensajeOV.java
@@ -0,0 +1,24 @@
+# Proyecto_TM
+RODRIGOCHINCHAY,JAMESCARBAJAL,KELLYVERONICA,JOSEZUÑIGA
+package com.example.proyecto_tm;
+
+public class MensajeVO {
+    private String name;
+    private String message;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
