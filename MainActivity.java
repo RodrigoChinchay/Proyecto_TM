a0f1a0ff56131cf07106f2b82f8fab99fc400bfb
jose-z-m <65821332+jose-z-m@users.noreply.github.com>
2020-07-17 12:36:09 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,25 +1,56 @@
-package com.example.usointent;
+package com.tmz.aplicacionzuniga;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    MediaPlayer mp;
+    int pause;
+    private Button buttonchat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonchat=findViewById(R.id.buttonchat);
+
+        buttonchat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-    public void onClickRegistro(View view) {
-        Intent Registro = new Intent(this,SegundaActivity.class);
-        startActivity(Registro);
+    public void play(View view){
+        if(mp==null) {
+            mp = MediaPlayer.create(this, R.raw.at);
+            mp.start();
+            Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show();
+        }else if(!mp.isPlaying()){
+            mp.seekTo(pause);
+            mp.start();
+            Toast.makeText(this, "Renaudando", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void pause(View view){
+        if(mp !=null) {
+            mp.pause();
+            pause =mp.getCurrentPosition();
+            Toast.makeText(this, "Pause", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void stop(View view){
+        mp.stop();
+        mp=null;
+        Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
     }
 
-    public void onClickLogin(View view) {
-        startActivity(new Intent("com.example.TerceraActivity"));
-    }
 }
